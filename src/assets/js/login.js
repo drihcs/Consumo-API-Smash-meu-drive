916f75194da0dd27b9891d8de3221fdf6ee5a6ee
Adriana Cruz <drih1502@gmail.com>
2025-05-05 00:15:50 +0000

    Atualiza login.js, remove dashboard antigo e adiciona novo dashboard

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if (loginForm) {
     loginForm.addEventListener("submit", function (e) {
       e.preventDefault();
-      window.location.href = "src/assets/components/dashboard.html";
+      window.location.href = "assets/dashboard.html";
     });
   }
 });
